95ec213f345b222c1ac86b05af1b3ce7553f4da3
Nelxn <nelsonasoh812@gmail.com>
2025-11-26 14:05:31 +0100

    Make app cloud-native with environment variables

---
+++ mysql-init/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS testdb;
+USE testdb;
+
+-- Sequelize will auto-create tables
+-- This script just ensures the database exists
--- node-express-server/server.js
+++ node-express-server/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: process.env.CORS_ORIGIN || "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
